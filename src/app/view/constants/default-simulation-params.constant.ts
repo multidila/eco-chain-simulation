3275e9c95f2d781cd56428d32ffe528ff0859361
Dmytro Nahaiko <d.nagayko@creatio.com>
2025-11-11 23:31:13 +0200

    fix: changes default simulation params

--- src/app/view/constants/default-simulation-params.constant.ts
+++ src/app/view/constants/default-simulation-params.constant.ts
@@ -8,18 +8,18 @@ export const DEFAULT_SIMULATION_PARAMS: SimulationParams = {
 		plant: { count: 100, energy: { initial: 100, max: 100 } },
 		herbivore: {
 			count: 50,
-			energy: { initial: 50, max: 100, metabolismRate: 5 },
+			energy: { initial: 100, max: 100, metabolismRate: 2 },
 			reproduction: { threshold: 0.9, shareRate: 0.5, mutationRate: 0.1, mutationStrength: 0.2 },
 			nutrition: {
-				[AgentType.Plant]: 15,
+				[AgentType.Plant]: 25,
 			},
 		},
 		carnivore: {
 			count: 25,
-			energy: { initial: 75, max: 150, metabolismRate: 3 },
+			energy: { initial: 100, max: 150, metabolismRate: 1 },
 			reproduction: { threshold: 0.9, shareRate: 0.5, mutationRate: 0.1, mutationStrength: 0.2 },
 			nutrition: {
-				[AgentType.Herbivore]: 30,
+				[AgentType.Herbivore]: 50,
 			},
 		},
 	},
